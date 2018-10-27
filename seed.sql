cdac065824c6acfd6f492d5c4c9e55cc9d6d1d9f
Eric Guerrero <kteacherbecomescoder@gmail.com>
2018-10-27 16:55:08 -0700

    MySQL Update

--- seed.sql
+++ seed.sql
@@ -2,18 +2,18 @@ USE bamazon_DB;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
-("Luke Skywalker BobbleHead Doll", "Collectibles", 19.99, 99),
-("Princess Leia BobbleHead Doll", "Collectibles", 19.99, 144),
-("Chewbacca BobbleHead Doll", "Collectibles", 19.99, 101),
-("Darth Vader BobbleHead Doll", "Collectibles", 19.99, 66),
-("BB-8 Pillow", "Bedding", 9.99, 500),
-("Porg Stuffed Plush", "Toys", 5.99, 100),
-("Kylo Ren Mask", "Costumes", 14.99, 181),
-("General Hux Speak and Spell", "Pure Entertainment", 24.99, 6),
-("Han Solo Replica Jacket", "Clothing", 99.99, 69),
-("C-3PO Talking Pez Dispensor", "Candy", 14.99, 77),
-("R2-D2 Remote Control Doll", "Electronics", 44.99, 33),
-("Purple Lightsaber", "Toys", 19.99, 55),
-("Rey Doll", "Collectibles", 21.99, 165);
+('Monopoly Board Game', 'Toys and Games', 24.99, 127),
+('Inflatable Halloween Yard Set', 'Outdoor', 79.99, 35),
+('Incredibles 2, Blu-Ray', 'Entertainment', 26.99, 27),
+('Pantene Pro V Shampoo', 'Health & Beauty', 11.99, 406),
+('The Girl Who Leapt Through Time, Blu-Ray', 'DVDs', 44.99, 30),
+('Buttered Popcorn, Jumbo Pack', 'Grocery', 19.95, 79),
+('Kibbles and Bits, Large Bag', 'Pets', 16.95, 47),
+('Pokemon, the Card Game', 'Toys and Games', 19.95, 41),
+('Han Solo Replica Jacket', 'Clothing', 99.99, 69),
+('C-3PO Talking Pez Dispensor', 'Candy', 14.99, 77),
+('iPad, 6th Gen.', 'Electronics', 454.99, 33),
+('Sorry Board Game', "Toys", 19.99, 55),
+('Batman Action Figure', "Collectibles", 21.99, 165);
 
 SELECT * FROM products;
