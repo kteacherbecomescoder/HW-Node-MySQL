45acfe672824657fd0968f8fa874b39657b9e6cc
Eric Guerrero <kteacherbecomescoder@gmail.com>
2018-10-27 16:40:48 -0700

    massive update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,58 +1,122 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql")
+const inquirer = require("inquirer")
 
-var connection = mysql.createConnection({
-    host: "localhost",
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
     port: 3306,
+
+    // Your username
     user: "root",
-    password: "password1",
-    database: "bamazonDB"
- })
 
-connection.connect(function(err){
-	if (err) throw (err);
-	console.log("connection successful!");
-	makeTable();
-})
+    // Your password
+    password: "password",
+    database: "bamazon_DB"
+});
 
-var makeTable = function(){
-	connection.query("SELECT * FROM products", function(err,res){
-		for(var i=0; i<res.length; i++){
-			console.log(res[i].itemId+" || "+res[i].productName+" || "+
-				res[i].departmentName+" || "+res[i].price+" || "+res[i].stockQuantity+"\n");
-		}
-	promptCustomer(res);
-	})
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err
+        // run the displayInventory function after the connection is made to prompt the user
+    displayInventory()
+});
+
+let displayInventory = () => {
+    //console.log("SQL Connection Established")
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err
+        for (let i = 0; i < res.length; i++) {
+            console.log(" - - - - - - - - - - - - - - - ")
+            console.log("item number: " + res[i].item_id)
+            console.log("item: " + res[i].product_name)
+            console.log("price: $" + res[i].price)
+        }
+        purchase()
+    })
+};
+
+// validateInput makes sure that the user is supplying only positive integers for their inputs
+let validateInput = (value) => {
+    var integer = Number.isInteger(parseFloat(value))
+    var sign = Math.sign(value)
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a whole non-zero number.'
+    }
 }
 
-var promptCustomer = function(res){
-	inquirer.prompt([{
-		type:"input",
-		name:"choice",
-		message:"What would you like to purchase?"
-	}]).then(function(answer){
-		var correct = false;
-		for(var i=0;i<res.length;i++){
-			if(res[i].productName==answer.choice){
-				correct = true;
-				var product=answer.choice;
-				var id=i;
-				inquirer.prompt({
-					type:"input",
-					name:"amount",
-					message:"How many would you like to purchase?",
-				}).then(function(answer){
-					if((res[id].stockQuantity-answer.amount)>0){
-						connection.query("UPDATE products SET stockQuantity="+(res[id].stockQuantity-answer.amount)+"' WHERE productName='"+product+"'", function(err,res2){
-							console.log("Item Purchased!");
-							makeTable();
-						})					
-					} else {
-						console.log("Unavailable!");
-						promptCustomer(res);
-					}
-				})
-			}
-		}
-	})
+// purchase function to prompt the customer for an item to purchase
+let purchase = () => {
+    inquirer.prompt([{
+                type: "input",
+                name: "item_id",
+                message: "Select the item you would like to purchase by item number.",
+                validate: validateInput,
+                filter: Number
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many of this item would you like to purchase?",
+                validate: validateInput,
+                filter: Number
+            }
+        ])
+        .then(function(purchase) {
+            let item = purchase.item_id
+            let quantity = purchase.quantity
+
+            let queryStr = 'SELECT * FROM products WHERE ?';
+
+            connection.query(queryStr, { item_id: item }, function(err, res) {
+                if (err) throw err
+
+                if (res.length === 0) {
+                    console.log("ERROR: Invalid Item ID. Please select a valid Item ID.")
+                    displayInventory()
+                } else {
+
+                    // set the results to the variable of productInfo
+                    let productInfo = res[0]
+
+                    if (quantity <= productInfo.stock_quantity) {
+                        console.log(productInfo.product_name + "is in stock! Placing order now!")
+                        console.log("\n")
+
+                        // the updating query string
+                        var updateQueryStr = "UPDATE products SET stock_quantity = " + (productInfo.stock_quantity - quantity) + " WHERE item_id = " + item
+                            // console.log('updateQueryStr = ' + updateQueryStr);
+
+                        // Update the inventory
+                        connection.query(updateQueryStr, function(err, data) {
+                            if (err) throw err;
+
+                            console.log("Your order has been placed!");
+                            console.log("Your total is $" + productInfo.price * quantity)
+                            console.log("Thank you for shopping with bamazon!")
+                            console.log(" - - - - - - - - - - - - - - - ")
+                            console.log("To shop again with us please input 'node bamazonCustomer.js' into your command line again.")
+                            console.log("\n")
+
+                            // End the database connection and close the app
+                            connection.end();
+                        })
+                    } else {
+                        console.log("Sorry, there is not enough " + productInfo.product_name + " in stock.")
+                        console.log("Your order can not be placed as is.")
+                        console.log("Please modify your order or select another item.")
+                        console.log("\n")
+
+                        // After 3 seconds display the inventory again so that the customer can make a new selcetion.
+                        setTimeout(function() { displayInventory() }, 3000)
+                    }
+
+
+                }
+            })
+
+
+        })
 }
